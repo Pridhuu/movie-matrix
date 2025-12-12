e6ca5218835c688db695cae7947583add4552492
Pridhuu <pridhuraj04@gmail.com>
2025-12-12 16:39:27 +0530

    Updated the structure

---
+++ frontend-moviematrix/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+    return <div className="home-page">
+        <h3>This is the Home page</h3>
+        <p>Will update it soon</p>
+    </div>
+}
+
+export default Home
--- frontend-moviematrix/src/pages/Movies.jsx
+++ frontend-moviematrix/src/pages/Movies.jsx
@@ -1,4 +1,7 @@
 import MovieCard from "../components/MovieCard"
+import {useState} from "react";
+import "../stylesheet/moviespage.css"
+import searchIcon from "../assets/search.svg"
 
 function Movies() {
 
@@ -6,19 +9,27 @@ function Movies() {
         {id: 1, title: "Terminator"},
         {id: 2, title: "Aquaman"},
         {id: 3, title: "Black Panther"},
+        {id: 4, title: "Ironman"},
+        {id: 5, title: "Avengers"},
+        {id: 6, title: "Yozuki"},
     ];
 
+    const [searchQuery, setSearchQuery] = useState("");
+
+    function onHandle(e) {
+        e.preventDefault()
+        alert(searchQuery);
+    }
+
     return <div className="movies-page">
-        <div className="Logo">
-            <img src="C:\Users\hpp\Desktop\Programming\GitRepository\movie-matrix\frontend-moviematrix\src\assets\MOVIE MATRIX.png" alt="LOGO" />
-        </div>
-        <div className="nav-bar"></div>
-        <div className="profile-section"></div>
         <form onSubmit={onHandle} className="search-form">
-            <input type="text" placeholder="Find your film." className="search-input"/>
+            <input type="text" placeholder="Find your film." className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+            <button type="submit" className="search-button">
+                <img src={searchIcon} alt="Search" height="18"/>
+            </button>
         </form>
         <div className="movie-grid">
-            {movies.map((item) => (<MovieCard movie={item} key={item.id}/>))}
+            {movies.map((item) => item.title.toLowerCase().startsWith(searchQuery) && (<MovieCard movie={item} key={item.id}/>))}
         </div>
     </div>
 }
