c706822c348d4c6ff6112b413aaeaa186c964b87
Pridhuu <pridhuraj04@gmail.com>
2025-12-12 16:32:43 +0530

    Added Movies Page

--- frontend-moviematrix/src/App.jsx
+++ frontend-moviematrix/src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
-import MovieCard from './components/MovieCard'
+import "./stylesheet/app.css";
+import Movies from "./pages/Movies";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <>
-      <MovieCard movie={{title: "Black Panther"}}></MovieCard>
-    </>
-  );  
+    <main className="whole-content">
+      <NavBar />
+      <div className="main-session">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Movies" element={<Movies />} />
+        </Routes>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
