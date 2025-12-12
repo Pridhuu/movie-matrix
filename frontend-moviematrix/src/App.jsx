c4ee3c1e2c73c530c1c56a8ec90979d4ef4fe674
Pridhuu <pridhuraj04@gmail.com>
2025-12-13 01:33:27 +0530

    Removed NavBar

--- frontend-moviematrix/src/App.jsx
+++ frontend-moviematrix/src/App.jsx
@@ -1,19 +1,23 @@
 import "./stylesheet/app.css";
 import Movies from "./pages/Movies";
+import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <main className="whole-content">
-      <NavBar />
+      {/* <NavBar /> */}
       <div className="main-session">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Movies" element={<Movies />} />
+          <Route path="/Favorites" element={<Favorites />} />
         </Routes>
       </div>
+      <Footer />
     </main>
   );
 }
